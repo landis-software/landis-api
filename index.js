da559bce245f9f2c3be03327d348821a6d214f20
Nick <66189083+urnotnick@users.noreply.github.com>
2022-01-27 13:26:18 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 /* Include the classes :) */
+const wrapper = require("api-wrapper");
+const bluebird = require("bluebird");
 export const Forum = require("./classes/Forum.js");
 export const ProfilePost = require("./classes/ProfilePost.js");
 export const ForumUser = require("./classes/ForumUser.js");
