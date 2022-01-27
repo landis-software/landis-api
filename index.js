d56f4efe70313a1b0263ea4ec0e411004692df02
Nick <66189083+urnotnick@users.noreply.github.com>
2022-01-27 13:07:47 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+/* Include the classes :) */
+export const Forum = require("./classes/Forum.js");
+export const ProfilePost = require("./classes/ProfilePost.js");
+export const ForumUser = require("./classes/ForumUser.js");
+export const Thread = require("./classes/Thread.js");
