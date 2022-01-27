ab0f27a684b978a692548b2a983ed1c686ca6cb3
Nick <66189083+urnotnick@users.noreply.github.com>
2022-01-27 12:57:36 -0600

    Create Forum.js

---
+++ classes/Forum.js
@@ -0,0 +1,59 @@
+export class Forum {
+    
+    get API() {
+        return this.__api;
+    }
+
+    __getUser(id, callback) {
+        forum.__api.getUser({
+            id: id
+        }, (_, __, userData) => {
+            callback(userData);
+        })
+    }
+
+    getThread(id,callback) {
+        return this.__api.getThread({
+            id: id
+        }, "", (a, b, body) => {
+            const author = new ForumUser(this, body.thread.user_id);
+            callback(author,body)
+        });
+    }
+
+    postToThread(id, message) {
+        msg = escape(message);
+        return this.API.postMessageAsync({
+            thread_id: id,
+            message: msg
+        }, "");
+    }
+
+    constructor(token) {
+        this.Token = token;
+        
+        this.__api = wrapper.create({
+            root: "https://forums.landis-community.com/api/",
+            parseJson: true,
+            requestDefaults: {
+                headers: { "XF-Api-Key": token }
+            },
+            get: {
+                getThreads: "threads/",
+                getThread: "threads/${id}/",
+                getUser: "users/${id}/",
+                getForum: "forums/${id}/threads/",
+                getMessage: "posts/${id}/"
+            },
+            post: {
+                makeProfilePost: "profile-posts/?user_id|message",
+                setUserName: "users/${id}/?username",
+                sendAlert: "alerts/",
+                postMessage: "posts/?thread_id|message",
+                updateThread: "threads/${id}/?prefix_id|title|discussion_open|sticky|custom_fields|add_tags|remove_tags",
+                setThreadTag: "threads/${id}/?custom_fields[${tag_name}]=${tag_value}"
+            }
+        })
+        this.__api = bluebird.promisifyAll(this.__api);
+    }
+};
